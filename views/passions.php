305a3ce19bcef3e86ca5c9e891d859d1fc38d8c7
Neguro <nassim.ahmedali@gmail.com>
2021-02-17 18:16:09 +0100

    ajout feature passions

---
+++ views/passions.php
@@ -0,0 +1,11 @@
+<div class="content-container">
+    <div class="container-fluid">
+        <div class="container" role="main">
+            <div class=".col-12 .col-sm-6 .col-xs-8">
+                <div class="jumbotron">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
