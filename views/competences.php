8c6224dd7fb3acee6245d1cf5a487fb45d996dc2
Neguro <nassim.ahmedali@gmail.com>
2021-02-17 18:14:31 +0100

    ajout feature competence

---
+++ views/competences.php
@@ -0,0 +1,105 @@
+<div class="content-container">
+            <div class="container-fluid">
+                <div class="container" role="main">
+                    <div class=".col-12 .col-sm-6 .col-xs-8">
+                      <div class="jumbotron">
+                            <h2 class="display-5 text-center mt-4">Mes compétences</h2>
+                            <hr class="my-4">
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    C#
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%;" aria-valuenow="0"
+                                    aria-valuemin="0" aria-valuemax="100">
+                                </div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    Java
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="25"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    Dev Web
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="50"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    SQL
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="75"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    Unity
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="100"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+
+                            <div class="row">
+                              <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    C#
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%;" aria-valuenow="0"
+                                    aria-valuemin="0" aria-valuemax="100">
+                                </div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    Java
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="25"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    Dev Web
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="50"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    SQL
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="75"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <br>
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
+                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
+                                    Unity
+                                </div>
+                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="100"
+                                    aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            </div>
+                      </div>      
+                    </div>
+                </div>
+            </div>
+        </div>
