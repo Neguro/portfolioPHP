fefced53d55555fc641c4321896ad46cf3649715
Neguro <nassim.ahmedali@gmail.com>
2021-02-17 18:18:40 +0100

    ajout feature projet

---
+++ views/projets.php
@@ -0,0 +1,64 @@
+<div class="content-container">
+    <div class="container-fluid">
+        <div class="jumbotron" style="background: transparent;">
+            <div class=".col-12 .col-sm-6 .col-xs-8">
+                <section id="gallery">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4 mb-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Portfolio</h5>
+                                        <p class="card-text">
+                                            Dépôt github de mon portfolio réaliser dans le cadre de mon BTS SIO,
+                                            déployer grâce a bootstrap CDN.
+                                        </p>
+                                        <a href="https://github.com/Neguro/MonPortfolio" class="btn btn-outline-success btn-sm">Lien dépôt
+                                            github</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 mb-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Build_Project</h5>
+                                        <p class="card-text">
+                                            Petit script d'automatisation de création d'un projet web symfony, réaliser
+                                            dans le cadre de mon BTS SIO afin de gagner du temps.
+                                        </p>
+                                        <a href="https://github.com/Neguro/build_project" class="btn btn-outline-success btn-sm">Lien dépôt
+                                            github</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 mb-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Chat serveur/client</h5>
+                                        <p class="card-text">
+                                            Projet réaliser en Java dans le but de tester la communication entre un client et un serveur,
+                                            pour un projet d'application android.
+                                        </p>
+                                        <a href="#" class="btn btn-outline-success btn-sm">Lien dépôt github</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 mb-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Service Windows</h5>
+                                        <p class="card-text">
+                                            Création d'un service windows éxecuter en tache de fond dans le cadre
+                                            d'un projet pour le BTS.
+                                        </p>
+                                        <a href="https://github.com/dussartcorp/-GSB_gestion_cloture" class="btn btn-outline-success btn-sm">Lien dépôt github</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
