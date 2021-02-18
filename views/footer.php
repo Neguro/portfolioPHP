71e12f21be6bc514dbf0dd90153929fab8177222
Neguro <nassim.ahmedali@gmail.com>
2021-02-18 15:24:54 +0100

    mise a jour de la date sur le footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
-    <div class="footer fixed-bottom" style="background-color: #375a7f; padding-top: 10px;">
+<div class="footer fixed-bottom" style="background-color: #375a7f; padding-top: 10px;">
                 <div class="container">
-                    <p style="text-align: center;"> <i class="fa fa-copyright" aria-hidden="true"></i> Nassim AHMED ALI - 2020 | Etudiant en BTS SIO | Mail pro : <a href="mailto:nassim.ahmedali@gmail.com">nassim.ahmedali@gmail.com </a> | Tout droits réserver</p>
+                    <p style="text-align: center;"> <i class="fa fa-copyright" aria-hidden="true"></i> Nassim AHMED ALI - <?php echo date("Y"); ?> | Tout droits réservés | Etudiant en BTS SIO | Mail pro : <a href="mailto:nassim.ahmedali@gmail.com">nassim.ahmedali@gmail.com </a></p>
                 </div>
             </div>
         </div>
