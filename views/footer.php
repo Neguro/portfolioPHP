a555904b98d1b065472ef4110e3b5249e10610a9
Neguro <nassim.ahmedali@gmail.com>
2021-02-18 08:58:04 +0100

    fix footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
     <div class="footer fixed-bottom" style="background-color: #375a7f;">
                 <div class="container">
-                    <p style="text-align: center;">Crée par Nassim AHMED ALI - 2020 | Etudiant en BTS SIO | Mail pro : <a href="mailto:nassim.ahmedali@gmail.com">nassim.ahmedali@gmail.com | Tout droits réserver </a></p>
+                    <p style="text-align: center;">Crée par Nassim AHMED ALI - 2020 | Etudiant en BTS SIO | Mail pro : <a href="mailto:nassim.ahmedali@gmail.com">nassim.ahmedali@gmail.com </a> | Tout droits réserver</p>
                 </div>
             </div>
         </div>
