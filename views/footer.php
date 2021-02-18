09f8f320734ac06f4bff6992f9c61e03bb757a66
Neguro <nassim.ahmedali@gmail.com>
2021-02-18 15:12:40 +0100

    ajout du logo copyright dans le footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
     <div class="footer fixed-bottom" style="background-color: #375a7f; padding-top: 10px;">
                 <div class="container">
-                    <p style="text-align: center;">Crée par Nassim AHMED ALI - 2020 | Etudiant en BTS SIO | Mail pro : <a href="mailto:nassim.ahmedali@gmail.com">nassim.ahmedali@gmail.com </a> | Tout droits réserver</p>
+                    <p style="text-align: center;"> <i class="fa fa-copyright" aria-hidden="true"></i> Nassim AHMED ALI - 2020 | Etudiant en BTS SIO | Mail pro : <a href="mailto:nassim.ahmedali@gmail.com">nassim.ahmedali@gmail.com </a> | Tout droits réserver</p>
                 </div>
             </div>
         </div>
