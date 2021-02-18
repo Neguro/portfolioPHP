67fb8d6a278492f111213e39cd54766615aa5f27
Neguro <nassim.ahmedali@gmail.com>
2021-02-18 11:03:06 +0100

    ajout carousel dans comptences

--- views/competences.php
+++ views/competences.php
@@ -1,104 +1,84 @@
 <div class="content-container">
-            <div class="container-fluid">
-                <div class="container" role="main">
-                    <div class=".col-12 .col-sm-6 .col-xs-8">
-                      <div class="jumbotron">
-                            <h2 class="display-5 text-center mt-4">Mes compétences</h2>
-                            <hr class="my-4">
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    C#
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%;" aria-valuenow="0"
-                                    aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-start">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    Java
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="25"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-start">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    Dev Web
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="50"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-start">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    SQL
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="75"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-start">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    Unity
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="100"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-
-                              <div class="progress col align-self-end">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    C#
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%;" aria-valuenow="0"
-                                    aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-end">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    Java
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="25"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-end">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    Dev Web
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="50"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-end">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    SQL
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="75"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <br>
-                            <div class="progress col align-self-end">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15"
-                                    aria-valuemax="100" style="width: 15%; background-color: grey;">
-                                    Unity
-                                </div>
-                                <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="100"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            </div>
-                      </div>      
+    <div class="container-fluid">
+        <div class="container" role="main">
+            <div class=".col-12 .col-sm-6 .col-xs-8">
+                <div class="jumbotron">
+                    <h2 class="display-5 text-center mt-4">Mes compétences</h2>
+                    <hr class="my-4">
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15" aria-valuemax="100" style="width: 15%; background-color: grey;">
+                            C#
+                        </div>
+                        <div class="progress-bar" role="progressbar" style="width: 35%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15" aria-valuemax="100" style="width: 15%; background-color: grey;">
+                            Java
+                        </div>
+                        <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15" aria-valuemax="100" style="width: 15%; background-color: grey;">
+                            Dev Web
+                        </div>
+                        <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15" aria-valuemax="100" style="width: 15%; background-color: grey;">
+                            SQL
+                        </div>
+                        <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="15" aria-valuemin="15" aria-valuemax="100" style="width: 15%; background-color: grey;">
+                            Unity
+                        </div>
+                        <div class="progress-bar" role="progressbar" style="width: 35%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="container">
+            <div class=".col-12 .col-sm-6 .col-xs-8">
+                <h2 class="display-5 text-center mt-4">Technos</h2>
+
+                <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="5000">
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+                </div>
+
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="../assets/images/angular.png" alt="Angular">
+                    </div>
+
+                    <div class="item">
+                        <img src="../assets/images/docker.png" alt="Docker">
+                    </div>
+
+                    <div class="item">
+                        <img src="../assets/images/git.png" alt="Git">
+                    </div>
+                </div>
+
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Précedent</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Suivant</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
