843cfef5ec2d40925ac8bdb077bbc7227a2fd725
Neguro <nassim.ahmedali@gmail.com>
2021-02-19 19:03:35 +0100

    mise a jour de la page veille

--- views/veille.php
+++ views/veille.php
@@ -3,6 +3,8 @@
         <div class="container" role="main">
             <div class=".col-12 .col-sm-6 .col-xs-8">
                 <div class="jumbotron">
+                    <h2 class="display-5 text-center mt-4">Veille</h2>
+                    <hr class="my-4">
                     <p>
                       La veille informative est une activité continue
                       en grande partie itérative visant à une surveillance active de l'environnement technologique, commercial, etc., 
@@ -10,13 +12,21 @@
                       Plutôt que de vous montrer un exemple de veille je vais vous donner quelques outils que je trouve essentiel selon moi 
                       pour pouvoir se tenir a jour sur le monde de l'informatique.
                     </p>
-                    <img class="text-center" src="../assets/images/dailydev.svg" width="200" height="200" alt="dailydev">
+                    <div class="container-fluid">
+                        <div class="text-center">
+                            <img class="center" src="../assets/images/dailydev.svg" width="200" height="200" alt="dailydev">
+                        </div>
+                    </div>
                     <p>
                         Excellent moyen pour ce tenir à jour sur le monde de l'informatique daily dev et une extention gratuite
                         disponible sur Google Chrome, Firefox, etc.
                         Daily dev met a disposition un dashbord avec des atricle partager par la communauté elle même. 
                     <p>
-                    <img class="text-center" src="../assets/images/feedly.svg" alt="feedly">
+                    <div class="container-fluid">
+                        <div class="text-center">
+                            <img class="center" src="../assets/images/feedly.svg" width="200" height="200" alt="feedly">
+                        </div>
+                    </div>
                     <p>
                         <!-- explication sur feedly --->
                     </p>
