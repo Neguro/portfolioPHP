9c18314e5a6f3c1cd34adf6c499dfd9820320c15
Neguro <nassim.ahmedali@gmail.com>
2021-02-24 10:43:48 +0100

    update mail.js

--- js/mail.js
+++ js/mail.js
@@ -1,6 +1,6 @@
 'use strict';
 
-$("#contactForm").submit(function (event) {
+document.getElementById("contactForm").submit(function (event) {
     event.preventDefault(); // annule l'action par défaut (on ne veut pas que la page se recharge)
     var form_url = $(this).attr("action"); // récupére l'URL du formulaire
     var form_method = $(this).attr("method"); //récupére la méthode du formulaire
@@ -12,12 +12,12 @@ $("#contactForm").submit(function (event) {
         method: form_method,
         data: form_data,
         success: function () {
-                $("#res").addClass('alert alert-success').html("Le mail a bien été envoyer !");
-                $("#contactForm").trigger("reset");
+                document.getElementById("res").addClass('alert alert-success').html("Le mail a bien été envoyer !");
+                document.getElementById("contactForm").trigger("reset");
         },
         error: function () {
-                $("#res").addClass('alert alert-danger').html("Il semblerait qu'il y ai un souci avec l'envoi de mail !");
-                $("#contactForm").trigger("reset");
+                document.getElementById("res").addClass('alert alert-danger').html("Il semblerait qu'il y ai un souci avec l'envoi de mail !");
+                document.getElementById("contactForm").trigger("reset");
         }
     })
 });
