e8ab1f41f182875dfa00d28a61f9703c2adc0677
Neguro <nassim.ahmedali@gmail.com>
2021-03-04 22:43:26 +0100

    mise en page de la page veille

--- views/veille.php
+++ views/veille.php
@@ -6,32 +6,60 @@
                     <h2 class="display-5 text-center mt-4">Veille</h2>
                     <hr class="my-4">
                     <p>
-                      La veille informative est une activité continue
-                      en grande partie itérative visant à une surveillance active de l'environnement technologique, commercial, etc., 
-                      pour en anticiper les évolutions.<br>
-                      Plutôt que de vous montrer un exemple de veille je vais vous donner quelques outils que je trouve essentiel selon moi 
-                      pour pouvoir se tenir a jour sur le monde de l'informatique.
+                        La veille informative est une activité continue
+                        en grande partie itérative visant à une surveillance active de l'environnement technologique, commercial, etc.,
+                        pour en anticiper les évolutions.
+                        Plutôt que de vous montrer un exemple de veille je vais vous donner quelques outils que je trouve essentiel selon moi
+                        pour pouvoir se tenir a jour sur le monde de l'informatique.
                     </p>
+                    <br>
+
                     <div class="container-fluid">
                         <div class="text-center">
-                            <img class="center" src="../assets/images/dailydev.svg" width="200" height="200" alt="dailydev">
+                            <img class="img-fluid" src="../assets/images/dailydev.svg" height="200" width="200" alt="dailydev">
                         </div>
                     </div>
+                    <br>
+
+                    <img class="img-fluid" src="../assets/images/daily_dev_ex.png" alt="dailydev">
+                    <br>
+
                     <p>
-                        Excellent moyen pour ce tenir à jour sur le monde de l'informatique daily dev et une extention gratuite
-                        disponible sur Google Chrome, Firefox, etc.
-                        Daily dev met a disposition un dashbord avec des atricle partager par la communauté elle même. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus ut tortor et accumsan.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                        Donec ultrices orci sit amet elit bibendum, at euismod eros posuere. Sed quis neque turpis.
+                        Ut at finibus ante. Phasellus aliquet vestibulum mi, ac sodales dolor. Nulla suscipit lorem lorem,
+                        non placerat sem vestibulum in. Donec at eleifend dolor. Morbi vitae varius lacus. Integer quis augue bibendum,
+                        porta mauris nec, pulvinar est. Praesent at arcu arcu. Cras porttitor velit nunc. Mauris nunc dolor, feugiat vel tristique a,
+                        tincidunt ac eros. Fusce iaculis viverra sem a dictum. Etiam vehicula, nisi sed convallis ultricies, lacus risus sodales sapien,
+                        malesuada finibus dolor eros nec turpis.
                     <p>
+
+                        <hr class="my-4">
+
                     <div class="container-fluid">
                         <div class="text-center">
-                            <img class="center" src="../assets/images/feedly.svg" width="200" height="200" alt="feedly">
+                            <img class="img-fluid" src="../assets/images/feedly.svg" height="200" width="200" alt="dailydev">
                         </div>
                     </div>
+                    <br>
+
+                    <img class="img-fluid" src="../assets/images/feedly_ex.png" alt="feedly">
+                    <br>
+
                     <p>
-                        <!-- explication sur feedly --->
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus ut tortor et accumsan.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                        Donec ultrices orci sit amet elit bibendum, at euismod eros posuere. Sed quis neque turpis.
+                        Ut at finibus ante. Phasellus aliquet vestibulum mi, ac sodales dolor. Nulla suscipit lorem lorem,
+                        non placerat sem vestibulum in. Donec at eleifend dolor. Morbi vitae varius lacus. Integer quis augue bibendum,
+                        porta mauris nec, pulvinar est. Praesent at arcu arcu. Cras porttitor velit nunc. Mauris nunc dolor, feugiat vel tristique a,
+                        tincidunt ac eros. Fusce iaculis viverra sem a dictum. Etiam vehicula, nisi sed convallis ultricies, lacus risus sodales sapien,
+                        malesuada finibus dolor eros nec turpis.
                     </p>
                 </div>
             </div>
         </div>
+        <br>
     </div>
 </div>
